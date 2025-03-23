9c82efac778baf6059bc9b05c80592bd4c602d77
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-03-23 08:47:04 +0530

    edits

--- app/account/page.js
+++ app/account/page.js
@@ -13,7 +13,7 @@ export default function Account() {
   return (
     <div className="flex h-screen flex-col items-start justify-start bg-neutral-100">
       <main className="w-full max-w-md">
-        <div className="bg-white p-4">
+        <div className="bg-white py-4 px-8">
           <h1 className="text-lg font-medium tracking-tight">Account Settings</h1>
         </div>
         <div className="h-full">
